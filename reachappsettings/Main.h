0f4cc68097161da820db07cdff748acbebc0e876
Shade-Zepheri <ziroalpha@gmail.com>
2017-10-23 20:23:16 -0500

    [Prefs] Fixed compiling error

--- reachappsettings/Main.h
+++ reachappsettings/Main.h
@@ -1,19 +1,20 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSListItemsController.h>
-#import <Preferences/PSViewController.h>
-#import <Preferences/PSSpecifier.h>
-#import <SettingsKit/SKListControllerProtocol.h>
-#import <SettingsKit/SKTintedListController.h>
-#import <Preferences/PSSwitchTableCell.h>
-#import <SettingsKit/SKStandardController.h>
-#import <SettingsKit/SKPersonCell.h>
-#import <SettingsKit/SKSharedHelper.h>
-#include <sys/sysctl.h>
-#include <sys/utsname.h>
 #import <AppList/AppList.h>
-#import <substrate.h>
 #import <notify.h>
 #import <libactivator/libactivator.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSListItemsController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSTableCell.h>
+#import <Preferences/PSViewController.h>
+#import <SettingsKit/SKListControllerProtocol.h>
+#import <SettingsKit/SKPersonCell.h>
+#import <SettingsKit/SKSharedHelper.h>
+#import <SettingsKit/SKStandardController.h>
+#import <Preferences/PSSwitchTableCell.h>
+#import <SettingsKit/SKTintedListController.h>
+#import <substrate.h>
+#include <sys/sysctl.h>
+#include <sys/utsname.h>
 #import <UIKit/UISearchBar.h>
 
 @interface PSViewController (SettingsKit2)
@@ -28,5 +29,5 @@
 @interface PSListItemsController (tableView)
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 - (void)listItemSelected:(id)arg1;
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+- (PSTableCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
 @end
