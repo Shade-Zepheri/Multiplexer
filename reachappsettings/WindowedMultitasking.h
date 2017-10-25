90e75ce92b329347a1027e948b83d295874a0a2a
Shade-Zepheri <ziroalpha@gmail.com>
2017-10-24 19:50:20 -0500

    [Prefs] Removed experimental prefs

--- reachappsettings/WindowedMultitasking.h
+++ reachappsettings/WindowedMultitasking.h
@@ -12,24 +12,7 @@
 @property (strong, nonatomic) ALApplicationTableDataSource *dataSource;
 @end
 
-@interface RAAlwaysWindowedAppsSelectorView : PSViewController <UITableViewDelegate>
-@property (strong, nonatomic) UITableView *tableView;
-@property (strong, nonatomic) ALApplicationTableDataSource *dataSource;
-@end
-
-@interface RAAlwaysLockedAppsSelectorView : PSViewController <UITableViewDelegate>
-@property (strong, nonatomic) UITableView *tableView;
-@property (strong, nonatomic) ALApplicationTableDataSource *dataSource;
-@end
 
 @interface RAApplicationTableDataSource : ALApplicationTableDataSource
-
-@end
-
-@interface RAWindowedApplicationTableDataSource : ALApplicationTableDataSource
-
-@end
-
-@interface RALockedApplicationTableDataSource : ALApplicationTableDataSource
 
 @end
